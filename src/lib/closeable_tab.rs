02e437b76cabbfb0aeea2f26d021e375aa14752d
Nefomemes <nefothingy@hotmail.com>
2021-11-09 05:18:23 +0700

    style: format source files with cargo fmt

--- src/lib/closeable_tab.rs
+++ src/lib/closeable_tab.rs
@@ -3,44 +3,32 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
 use glib::IsA;
-use gtk::{prelude::*, Box, Widget, Button};
+use gtk::{prelude::*, Box, Button, Widget};
 
 pub trait ClosableTabImplementedNotebook {
-    fn prepend_closable_page<
-        T: IsA<Widget>, 
-        U: IsA<Widget>
-    >(
+    fn prepend_closable_page<T: IsA<Widget>, U: IsA<Widget>>(
         &self,
         child: &T,
         tab_label: Option<&U>,
     ) -> u32;
-    fn append_closable_page<
-        T: IsA<Widget>, 
-        U: IsA<Widget>
-    >(
+    fn append_closable_page<T: IsA<Widget>, U: IsA<Widget>>(
         &self,
         child: &T,
         tab_label: Option<&U>,
     ) -> u32;
 
-    fn create_closable_tab<
-        U: IsA<Widget>,
-
-    >(tab_label: Option<&U>) -> (Box, Button);
+    fn create_closable_tab<U: IsA<Widget>>(tab_label: Option<&U>) -> (Box, Button);
 }
 
 impl ClosableTabImplementedNotebook for gtk::Notebook {
-    fn create_closable_tab<
-    U: IsA<Widget>,
-
->(tab_label: Option<&U>)-> (Box, Button) {
+    fn create_closable_tab<U: IsA<Widget>>(tab_label: Option<&U>) -> (Box, Button) {
         let tab = Box::new(gtk::Orientation::Horizontal, 5);
         if tab_label.is_some() {
             tab.append(tab_label.unwrap());
         }
 
         let button = gtk::Button::new();
-        
+
         button.set_icon_name("window-close-symbolic");
         button.set_has_frame(false);
 
@@ -57,19 +45,15 @@ impl ClosableTabImplementedNotebook for gtk::Notebook {
         let (tab, button) = &Self::create_closable_tab(tab_label);
         let page = self.prepend_page(child, Some(tab));
 
-        button.connect_clicked(glib::clone!(@weak self as notebook => 
+        button.connect_clicked(glib::clone!(@weak self as notebook =>
             move |_| {
             notebook.remove_page(Some(page));
         }));
 
         page
-
     }
 
-    fn append_closable_page<
-        T: IsA<Widget>, 
-        U: IsA<Widget>
-    >(
+    fn append_closable_page<T: IsA<Widget>, U: IsA<Widget>>(
         &self,
         child: &T,
         tab_label: Option<&U>,
@@ -77,7 +61,7 @@ impl ClosableTabImplementedNotebook for gtk::Notebook {
         let (tab, button) = &Self::create_closable_tab(tab_label);
         let page = self.append_page(child, Some(tab));
 
-        button.connect_clicked(glib::clone!(@weak self as notebook => 
+        button.connect_clicked(glib::clone!(@weak self as notebook =>
             move |_| {
             notebook.remove_page(Some(page));
         }));
