8b5781b180e9bf2d068f61a26faaa6345df17f5a
Nefo Fortressia <nefothingy@hotmail.com>
2022-01-29 15:53:56 +0700

    refactor: change all error return types to dyn Error
    We still use panic! and Option::expect() a lot in our code.
    
    The intended practice in Rust is to not panic, but to return an error.
    
    Some of our functions already do this, but they return &str
    and not dynstd::error::Error intended by Rust.
    So this commit changes that to dyn std::error::Error.
    
    Functions changed in this commit will no longer panic when being called.
    When getting an error when run, methods now return Err<Box<dyn Error>>,
    while constructors now return Err<BoolError>.
    Event handlers can still panic however.
    
    See https://doc.rust-lang.org/book/ch09-02-recoverable-errors-with-result.html

--- editor/src/components/editor/mod.rs
+++ editor/src/components/editor/mod.rs
@@ -3,6 +3,8 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
 pub mod imp;
+use std::{error::Error, fmt};
+
 use gio::Cancellable;
 use gtk::prelude::*;
 use gtk::subclass::prelude::*;
@@ -18,68 +20,73 @@ glib::wrapper! {
 }
 
 impl EchidnaCoreEditor {
-    pub fn new(file: Option<sourceview::File>) -> Self {
-        let this: Self =
-            glib::Object::new(&[]).expect("Failed to create 'EchidnaCoreEditor' component.");
-        let this_imp = this.to_imp();
-        // Without cloning it, for some reasons the Rust compiler complains about &this.to_imp().sourceview not being IsA<sourceview::View>
-        this_imp.minimap.set_view(&this_imp.sourceview.clone());
+    pub fn new(file: Option<sourceview::File>) -> Result<Self, glib::BoolError> {
+        let this: Result<Self, glib::BoolError> = glib::Object::new(&[]);
 
-        match file {
-            Some(file) => {
-                let file_location = file
-                    .location()
-                    .expect("file is required to have a location");
+        match this {
+            Ok(this) => {
+                let this_imp = this.to_imp();
+                // Without cloning it, for some reasons the Rust compiler complains about &this.to_imp().sourceview not being IsA<sourceview::View>
+                this_imp.minimap.set_view(&this_imp.sourceview.clone());
 
-                this.set_property("file", &file)
-                    .expect("Could not set the 'file' property of EchidnaCoreEditor");
+                match file {
+                    Some(file) => {
+                        let file_location = file
+                            .location()
+                            .expect("file is required to have a location");
 
-                let cancellable = gio::Cancellable::new();
-                let filepath = file_location.path().expect("No filepath");
-                let info = file_location
-                    .query_info("*", gio::FileQueryInfoFlags::NONE, Some(&cancellable))
-                    .expect("Could not query the info for file");
+                        this.set_property("file", &file)
+                            .expect("Could not set the 'file' property of EchidnaCoreEditor");
 
-                let content_type = info
-                    .content_type()
-                    .expect(format!("It does not seem like {:?} has a type", filepath).as_str());
-                {
-                    println!(
-                        "Opened {} and found its content type is {}.",
-                        "file",
-                        content_type.to_string()
-                    );
-                    let buffer = this_imp.sourceview.buffer().downcast::<Buffer>().expect("Cannot downcast the sourceview's buffer. Maybe the sourceview's buffer is not IsA<sourceview::Buffer>.");
-                    let language_manager = LanguageManager::new();
-                    let language = language_manager.guess_language(
-                        Some(&info.name().to_str().expect(
-                            "Could not open the file because its name is not supported by Unicode.",
-                        )),
-                        None,
-                    );
+                        let cancellable = gio::Cancellable::new();
+                        let filepath = file_location.path().expect("No filepath");
+                        let info = file_location
+                            .query_info("*", gio::FileQueryInfoFlags::NONE, Some(&cancellable))
+                            .expect("Could not query the info for file");
 
-                    match language {
-                        Some(lang) => buffer.set_language(Some(&lang)),
-                        None => {}
-                    }
+                        let content_type = info.content_type().expect(
+                            format!("It does not seem like {:?} has a type", filepath).as_str(),
+                        );
+                        {
+                            println!(
+                                "Opened {} and found its content type is {}.",
+                                "file",
+                                content_type.to_string()
+                            );
+                            let buffer = this_imp.sourceview.buffer().downcast::<Buffer>().expect("Cannot downcast the sourceview's buffer. Maybe the sourceview's buffer is not IsA<sourceview::Buffer>.");
+                            let language_manager = LanguageManager::new();
+                            let language = language_manager.guess_language(
+                                Some(&info.name().to_str().expect(
+                                    "Could not open the file because its name is not supported by Unicode.",
+                                )),
+                                None,
+                            );
 
-                    let file_loader: FileLoader = FileLoader::new(&buffer, &file);
+                            match language {
+                                Some(lang) => buffer.set_language(Some(&lang)),
+                                None => {}
+                            }
 
-                    file_loader.load_async(
-                    glib::Priority::default(),
-                    Some(&cancellable),
-                    |_, _| {},
-                    |result| {
-                        if result.is_err() {
-                            panic!("Found an error when loading the file into the text editor's buffer. {:#?}", result.err());
+                            let file_loader: FileLoader = FileLoader::new(&buffer, &file);
+
+                            file_loader.load_async(
+                            glib::Priority::default(),
+                            Some(&cancellable),
+                            |_, _| {},
+                            |result| {
+                                if result.is_err() {
+                                    panic!("Found an error when loading the file into the text editor's buffer. {:#?}", result.err());
+                                }
+                            },
+                        );
                         }
-                    },
-                );
+                    }
+                    None => {}
                 }
+                Ok(this)
             }
-            None => {}
+            Err(e) => Err(e),
         }
-        this
     }
 
     pub fn to_imp(&self) -> &imp::EchidnaCoreEditor {
@@ -90,7 +97,7 @@ impl EchidnaCoreEditor {
         self.property("file").expect("Could not get property 'file' of EchidnaCoreEditor").get::<sourceview::File>().expect("Could not get property 'file' of EchidnaCoreEditor because its type is not IsA<sourceview::File>")
     }
 
-    pub fn save_file(&self, save_as: Option<&gio::File>) -> Result<(), &str> {
+    pub fn save_file(&self, save_as: Option<&gio::File>) -> Result<(), Box<dyn Error>> {
         let window_imp = self.to_imp();
         let buffer = self.to_imp().sourceview.buffer().downcast::<Buffer>();
 
@@ -98,7 +105,7 @@ impl EchidnaCoreEditor {
             Ok(buffer) => {
                 let cancellable = Cancellable::new();
                 let mut file_saver: Option<FileSaver> = None;
-                let result: Result<(), &str> = match save_as {
+                let result: Result<(), Box<dyn Error>> = match save_as {
                     Some(file) => {
                         file_saver = Some(FileSaver::with_target(&buffer, &self.file(), file));
                         Ok(())
@@ -108,7 +115,19 @@ impl EchidnaCoreEditor {
                             file_saver = Some(FileSaver::new(&buffer, &self.file()));
                             Ok(())
                         }
-                        None => Err("The file location must exist. Please do \"Save As\""),
+                        None => {
+                            #[derive(Debug)]
+                            struct Error {}
+
+                            impl fmt::Display for Error {
+                                fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+                                    write!(f, "The file location must exist. Please do \"Save As\"")
+                                }
+                            }
+
+                            impl std::error::Error for Error {}
+                            Err(Box::new(Error {}))
+                        }
                     },
                 };
 
@@ -132,7 +151,20 @@ impl EchidnaCoreEditor {
                     }
                 }
             }
-            Err(_) => Err("Can't downcast the buffer to GtkSourceBuffer."),
+            Err(_) => {
+                #[derive(Debug)]
+                struct Error {}
+
+                impl fmt::Display for Error {
+                    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+                        write!(f, "Can't downcast the buffer to GtkSourceBuffer.")
+                    }
+                }
+
+                impl std::error::Error for Error {}
+
+                Err(Box::new(Error {}))
+            }
         }
     }
 }
