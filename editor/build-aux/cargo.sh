ddffb47db4a472b2657d34a0556dfb39e9f0251a
Nefo Fortressia <nefothingy@hotmail.com>
2022-05-26 06:00:37 +0700

    build: just expose the ~/.cargo directory to Flatpak
    Duh!

--- editor/build-aux/cargo.sh
+++ editor/build-aux/cargo.sh
@@ -4,13 +4,6 @@ export MESON_BUILD_ROOT="$1"
 CARGO_HOME="~/.cargo"
 export MESON_SOURCE_ROOT="$2"
 export CARGO_TARGET_DIR="$MESON_BUILD_ROOT"/target
-if test -r $CARGO_HOME -a -w $CARGO_HOME || test -w "~"
-then
-echo "It seem we can use the host's Cargo home."
-else
-echo "It seems we can't use the host's Cargo home due to the permissions policy. This usually happens under sandboxes like Flatpak or Firejail. Setting the Cargo home to $MESON_BUILD_ROOT/cargo-home."
-export CARGO_HOME="$MESON_BUILD_ROOT"/cargo-home
-fi
 export OUTPUT="$3"
 export BUILDTYPE="$4"
 export APP_BIN="$5"
