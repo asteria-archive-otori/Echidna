bea352936d468c686a81150948917718aa7b4b50
Nefo Fortressia <nefothingy@hotmail.com>
2022-06-10 18:32:34 +0700

    refactor: move EchidnaCoreEditor::new() code to constructed

--- editor/src/components/editor/imp.rs
+++ editor/src/components/editor/imp.rs
@@ -1,14 +1,23 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
+/* obj Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with obj
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
 use crate::prelude::*;
-use glib::{ParamFlags, ParamSpec, ParamSpecObject, Value};
+use glib::{ParamFlags, ParamSpec, ParamSpecObject, Value, clone};
 
 use gtk::{subclass::prelude::*, CompositeTemplate};
 use once_cell::sync::Lazy;
-use std::cell::RefCell;
-
+use sourceview::{traits::*, Buffer, LanguageManager, FileLoader};
+use std::{cell::RefCell};
+fn set_scheme(buffer: &sourceview::Buffer, is_dark: bool) {
+    let style_manager = sourceview::StyleSchemeManager::default();
+    // Set the scheme to Adwaita by default.
+    let default_scheme = match is_dark {
+        true => "Adwaita-dark",
+        false => "Adwaita",
+    };
+    buffer.set_style_scheme(style_manager.scheme(default_scheme).as_ref());
+}
 #[derive(Default, CompositeTemplate)]
 #[template(resource = "/io/fortressia/Echidna/components/editor/editor.ui")]
 pub struct EchidnaCoreEditor {
@@ -16,7 +25,8 @@ pub struct EchidnaCoreEditor {
     pub minimap: TemplateChild<sourceview::Map>,
     #[template_child]
     pub sourceview: TemplateChild<sourceview::View>,
-    pub file: RefCell<Option<sourceview::File>>,
+    file: RefCell<Option<sourceview::File>>,
+    app: RefCell<Option<adw::Application>>,
 }
 
 #[glib::object_subclass]
@@ -35,6 +45,70 @@ impl ObjectSubclass for EchidnaCoreEditor {
 }
 
 impl ObjectImpl for EchidnaCoreEditor {
+
+    fn constructed(&self, obj: &Self::Type) {
+           // Without cloning it, for some reasons the Rust compiler complains about &obj.to_imp().sourceview not being IsA<sourceview::View>
+           self.minimap.set_view(&self.sourceview.clone());
+           let buffer = self.sourceview.buffer().downcast::<Buffer>().expect("Cannot downcast the sourceview's buffer. Maybe the sourceview's buffer is not IsA<sourceview::Buffer>.");
+           
+           if let Some(f) = obj.file() {
+               let file_location = f.location();
+               obj.set_property("file", &f);
+   
+               let cancellable = gio::Cancellable::new();
+               let filepath = file_location.path().expect("No filepath");
+               let info = file_location
+                   .query_info("*", gio::FileQueryInfoFlags::NONE, Some(&cancellable))
+                   .expect("Could not query the info for file");
+   
+               let content_type = info.content_type().expect(
+                   format!("It does not seem like {:?} has a type", filepath).as_str(),
+               );
+               {
+                   println!(
+                       "Opened {} and found its content type is {}.",
+                       "file",
+                       content_type.to_string()
+                   );
+   
+                   let language_manager = LanguageManager::new();
+                   let language = language_manager.guess_language(
+                       Some(&info.name().to_str().expect(
+                           "Could not open the file because its name is not supported by Unicode.",
+                       )),
+                       None,
+                   );
+   
+                   if let Some(lang) = language {
+                       buffer.set_language(Some(&lang));
+                   }
+   
+                   let file_loader: FileLoader = FileLoader::new(&buffer, &f);
+   
+                   file_loader.load_async(
+                   glib::Priority::default(),
+                   Some(&cancellable),
+   
+                   |result| {
+                       if result.is_err() {
+                           panic!("Found an error when loading the file into the text editor's buffer. {:#?}", result.err());
+                       }
+                   },
+                   );
+               }
+           }
+   
+           if let Some(a) = obj.app() {
+               let manager = a.style_manager();
+               set_scheme(&buffer, manager.is_dark());
+               manager.connect_dark_notify(clone!(@weak buffer =>
+                   move |manager|{
+                   set_scheme(&buffer, manager.is_dark());
+               }));
+           }
+           
+           
+    }
     fn properties() -> &'static [ParamSpec] {
         static PROPERTIES: Lazy<Vec<ParamSpec>> = Lazy::new(|| {
             vec![ParamSpecObject::new(
@@ -43,7 +117,16 @@ impl ObjectImpl for EchidnaCoreEditor {
                 "the file of the editor",
                 sourceview::File::static_type(),
                 ParamFlags::READWRITE,
-            )]
+            ),
+            ParamSpecObject::new(
+                "file",
+                "file",
+                "the application",
+                adw::Application::static_type(),
+                ParamFlags::READWRITE
+            )
+            
+            ]
         });
 
         PROPERTIES.as_ref()
@@ -56,6 +139,11 @@ impl ObjectImpl for EchidnaCoreEditor {
                     value.get().expect("The file needs to be sourceview::File");
                 self.file.replace(file);
             }
+            "app" => {
+                let app: Option<adw::Application> =
+                    value.get().expect("The file needs to be adw::Application");
+                self.app.replace(app);
+            }
             _ => unimplemented!(),
         }
     }
@@ -63,9 +151,11 @@ impl ObjectImpl for EchidnaCoreEditor {
     fn property(&self, _obj: &Self::Type, _id: usize, spec: &ParamSpec) -> Value {
         match spec.name() {
             "file" => self.file.borrow().to_value(),
+            "app" => self.app.borrow().to_value(),
             _ => unimplemented!(),
         }
     }
 }
+
 impl WidgetImpl for EchidnaCoreEditor {}
 impl BoxImpl for EchidnaCoreEditor {}
